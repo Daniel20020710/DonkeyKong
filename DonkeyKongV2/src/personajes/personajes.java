cbf3765012e03f57033b3267105d682bd0ea6525
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2022-11-22 00:02:37 -0500

    Nuevo

---
+++ DonkeyKongV2/src/personajes/personajes.java
@@ -0,0 +1,26 @@
+package personajes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JLabel;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author RANDY
+ */
+public class personajes {
+
+    public javax.swing.JLabel ob_label;
+    public int x;
+
+    public personajes(JLabel ob_label) {
+        this.ob_label = ob_label;
+    }
+    
+
+}
