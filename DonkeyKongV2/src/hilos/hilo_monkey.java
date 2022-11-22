6da832ab8ff21dbaf9e860663fdd8a5fab195307
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-11-22 01:13:52 -0500

    subir proyect

---
+++ DonkeyKongV2/src/hilos/hilo_monkey.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hilos;
+
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import personajes.monkey;
+
+/**
+ *
+ * @author Usuario
+ */
+public class hilo_monkey extends Thread {
+
+    monkey donkeykong;
+    int velocidad = 10;
+
+    public hilo_monkey(monkey donkeykong) {
+        this.donkeykong = donkeykong;
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        while (true) {
+
+
+            try {
+                derecha(donkeykong.ob_label);
+                izquierda(donkeykong.ob_label);
+                sleep(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(monkey.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+    public void derecha(JLabel label) throws InterruptedException {
+        int x = 100;
+        while (x <= 350) {
+
+            label.setLocation(x, label.getY());
+            x++;
+            sleep(velocidad);
+        }
+    }
+
+    public void izquierda(JLabel label) throws InterruptedException {
+        int x = 350;
+        while (x >= 100) {
+
+            label.setLocation(x, label.getY());
+            x--;
+            sleep(velocidad);
+        }
+    }
+
+}
